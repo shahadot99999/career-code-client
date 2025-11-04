5c2067011446ff9f76ae2c3bf72ec9247bd65ac2
shahadot99999 <shahadot99999@gmail.com>
2025-11-04 18:35:59 +0600

    Banner Image Animation using React Motion

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { motion } from "motion/react"
+import team1 from '../../assets/team/01.jpg';
+import team2 from '../../assets/team/02.jpg'
 
 const Banner = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero bg-base-200 min-h-96">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                    className="max-w-sm rounded-lg shadow-2xl"
+                <div className='flex-1'>
+                    <motion.img
+                    src={team1}
+                    animate={{y: [100, 150, 100]}}
+                    transition={{duration: 5, repeat: Infinity}}
+                    className="max-w-sm border-blue-500 border-s-8 border-b-8 rounded-t-[40px] rounded-b-[40px] shadow-2xl"
                 />
-                <div>
-                    <motion.h1
+                 <motion.img
+                    src={team2}
+                    animate={{x: [100, 150, 100]}}
+                    transition={{duration: 10, delay:5, repeat: Infinity}}
+                    className="max-w-sm border-blue-500 border-s-8 border-b-8 rounded-t-[40px] rounded-b-[40px] shadow-2xl"
+                />
+                </div>
+                <div className='flex-1'>
+                    {/* <motion.h1
                     animate={{
                          rotate: 180,
+                         x: 200,
+                         y: -200,
                         transition: { duration: 4 }
                         }}
-                    className="text-5xl font-bold">Lattest jobs for you!</motion.h1>
+                    className="text-5xl font-bold">Lattest jobs for you!</motion.h1> */}
 
                      <motion.h1
                     initial={{ scale: 0 }} 
