e358b5601c065e6407dab4ba1db1572c51e75b18
shahadot99999 <shahadot99999@gmail.com>
2025-11-18 18:03:30 +0600

    Show my Application in Table

--- src/pages/MyApplications/ApplicationList.jsx
+++ src/pages/MyApplications/ApplicationList.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { use } from 'react';
+import JobApplicationRow from './JobApplicationRow';
 
-const ApplicationList = () => {
+const ApplicationList = ({myApplicationsPromise}) => {
+    const applications = use(myApplicationsPromise)
+
     return (
         <div>
-            
+           <h3 className="text-3xl">Jobs Applied so far: {applications.length}</h3> 
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       {
+                        applications.map((application, index)=> <JobApplicationRow
+                        key={application._id}
+                        index={index}
+                        application={application}
+                        ></JobApplicationRow>)
+                       }
+                    </tbody>
+                   
+                </table>
+            </div>
         </div>
     );
 };
--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -1,13 +1,22 @@
 import React, { Suspense } from 'react';
 import ApplicationStarts from './ApplicationStarts';
 import ApplicationList from './ApplicationList';
+import useAuth from '../../hooks/useAuth';
+import { myApplicationsPromise } from '../../api/applicationsApi';
+
+
+
 
 const MyApplications = () => {
+
+    const {user}= useAuth();
     return (
         <div>
           <ApplicationStarts></ApplicationStarts>  
           <Suspense fallback={'loading your applications'}>
-            <ApplicationList></ApplicationList>
+            <ApplicationList
+            myApplicationsPromise={myApplicationsPromise(user.email)}
+            ></ApplicationList>
           </Suspense>
         </div>
     );
