e7de1f2d64981622caeed565ac145bb999da61a7
shahadot99999 <shahadot99999@gmail.com>
2025-11-19 18:22:41 +0600

    Create large form with select for Add Form

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -5,6 +5,113 @@ const AddJob = () => {
         <div>
             <h3 className="3xl">Please add a job.</h3>
             <form >
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Basic Info</legend>
+
+                    <label className="label">Job Title</label>
+                    <input type="text" name='title' className="input" placeholder="Job Title" />
+
+                    <label className="label">Company</label>
+                    <input type="text" name='company' className="input" placeholder="Company Name" />
+
+                    <label className="label">Location</label>
+                    <input type="text" name='location' className="input" placeholder="Company Location" />
+
+                    <label className="label">Company logo</label>
+                    <input type="text" name='company_logo' className="input" placeholder="Company Logo Url" />
+                </fieldset>
+
+                {/* Job Type */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Job Type</legend>
+                    <div className="filter">
+                        <input className="btn filter-reset" type="radio" name="jobType" aria-label="All" />
+                        <input className="btn" type="radio" name="jobType" aria-label="On-site" />
+                        <input className="btn" type="radio" name="jobType" aria-label="Remote" />
+                        <input className="btn" type="radio" name="jobType" aria-label="Hybrid" />
+                    </div>
+                  
+                </fieldset>
+
+                {/* Job Category */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Job Category</legend>
+
+                    <select defaultValue="Job Category" name='category'
+                     className="select">
+                        <option disabled={true}>Job Category</option>
+                        <option>Engineering</option>
+                        <option>Marketing</option>
+                        <option>Finance</option>
+                    </select>
+                </fieldset>
+
+                {/*Application Deadline */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Application Deadline</legend>
+                    <input type="date" className="input" />
+
+                  
+                </fieldset>
+
+                {/* Salary Range */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Salary Range</legend>
+
+                    <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+
+                        <div>
+                            <label className="label">Minimum Salary</label>
+                            <input type="text" className="input" name='salaryMin' placeholder="Minimum Salary" />
+                        </div>
+
+                        <div>
+                            <label className="label">Maximum Salary</label>
+                            <input type="text" className="input" name='salaryMax'
+                                placeholder="Maximum Salary" />
+                        </div>
+
+                        <div>
+                            <label className="label">Currency</label>
+                            <select defaultValue="Select a Currency" name='currency category'
+                                className="select">
+                                <option disabled={true}>Select a Currency</option>
+                                <option>BDT</option>
+                                <option>USD</option>
+                                <option>EU</option>
+                            </select>
+                        </div>
+                    </div>
+
+                </fieldset>
+
+                {/*  */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Salary Range</legend>
+
+                  
+                </fieldset>
+
+                {/*  */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Basic Info</legend>
+
+                  
+                </fieldset>
+
+                {/*  */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Basic Info</legend>
+
+                  
+                </fieldset>
+
+                {/*  */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Basic Info</legend>
+
+                  
+                </fieldset>
 
             </form>
         </div>
