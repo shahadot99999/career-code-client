f4dc361097b5bf096ca1164acfd0c248cfb6497c
shahadot99999 <shahadot99999@gmail.com>
2025-11-24 18:22:16 +0600

    Show My Posted Jobs And Count Job Applications client side

--- src/pages/MyPostrdJobs/MyPostrdJobs.jsx
+++ src/pages/MyPostrdJobs/MyPostrdJobs.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import useAuth from '../../hooks/useAuth';
+import JobLists from './JobLists';
+import { jobsCreatedByPromise } from '../../api/jobsApi';
 
 const MyPostedJobs = () => {
+    const {user}= useAuth();
     return (
         <div>
             <h2>My posted Jobs:</h2>
+            <Suspense>
+                <JobLists
+                  jobsCreatedByPromise={jobsCreatedByPromise(user.email)}
+                ></JobLists>
+            </Suspense>
         </div>
     );
 };
