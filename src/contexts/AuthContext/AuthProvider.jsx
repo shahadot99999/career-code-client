5ccfc6b8600225ba06915206693f7f95d00e44ec
shahadot99999 <shahadot99999@gmail.com>
2025-11-30 22:10:12 +0600

    create sample jwt token  and use axios to get the token client side

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
+import axios from 'axios';
 
 
 
@@ -39,6 +40,16 @@ const AuthProvider = ({children}) => {
         const unSubscibe = onAuthStateChanged(auth, currentUser =>{
             setUser(currentUser);
             setLoading(false);
+            if(currentUser?.email){
+                const userData= { email: currentUser.email}
+                axios.post('http://localhost:3000/jwt', userData)
+                .then(res=>{
+                    console.log('token after jwt', res.data);
+                    // const token = res.data.token;
+                    // localStorage.setItem('token', token);
+                })
+                .catch(error=>console.log(error))
+            }
             console.log('user in the auth state change', currentUser);
         } )
         return ()=>{
